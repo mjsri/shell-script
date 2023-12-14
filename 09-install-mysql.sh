6e86c0b095dfb4da9e384a1dfd69886f12cd4f54
mjsri <srikanthchowdary1994@gmail.com>
2023-12-14 16:45:16 +0530

    added 3 shell variable

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -5,8 +5,9 @@ ID=$(id -u)
 if [ $ID -ne 0 ]
 then 
    echo "ERROR:: Please run this script in root user access"
+   exit 1 # you can give other than 0
 else
    echo "you are root user"
-fi
+fi  # fi means end of if means end of statement
 
 yum install mysql -y
