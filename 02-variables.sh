017383494c2d8c9b590c7a6483c0ef1d9c56d715
mjsri <srikanthchowdary1994@gmail.com>
2024-06-18 17:03:50 +0530

    shell practice second time practice

--- 02-variables.sh
+++ 02-variables.sh
@@ -6,4 +6,4 @@ Person2=Suresh
 echo "$Person1: Hello $Person2, Good morning"
 echo "$Person2: Hi $Person1,very good morning"
 echo "$Person1: How are you $Person2"
-echo "$Person2: I am good $Person1, How are you"           
+echo "$Person2: I am good $Person1, How are you bro"           
