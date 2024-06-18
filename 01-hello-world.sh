26a2a53947593ec58009a2d30fe849aaa2938aac
mjsri <srikanthchowdary1994@gmail.com>
2024-06-18 17:01:33 +0530

    shell practice second time practice

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -2,4 +2,4 @@
 
 #otherthan first line if #is present in other lines means it is commented out
 
-echo "Hello shell script (Pritnting some thing) for second time practice"
+echo "Hello shell script (Pritnting some thing) for second time practice of shell"
