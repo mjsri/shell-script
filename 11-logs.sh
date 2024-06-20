e24086b626d9fe20e2d05acbef68e87d7f26e0d5
mjsri <srikanthchowdary1994@gmail.com>
2024-06-20 16:42:36 +0530

    shell practice second time practice

--- 11-logs.sh
+++ 11-logs.sh
@@ -2,6 +2,9 @@
 
 ID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
@@ -10,16 +13,16 @@ echo "Script started  executing at $TIMESTAMP" &>>LOGFILE
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "ERROR:: $2 .... failed"
+        echo -e "ERROR:: $2 .... $R failed $N"
         exit 1
     else
-        echo "$2 .....success"
+        echo -e "$2 ..... $G success $N"
     fi
 }
 
 if [ $ID -ne 0 ]
 then 
-   echo "ERROR:: Please run this script in root user access"
+   echo -e "$R ERROR:: Please run this script in root user access $N"
    exit 1 # you can give other than 0
 else
    echo "you are root user"
