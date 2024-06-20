604bb9ebdd0d6c83d964be4d7be46f62fe3276f2
mjsri <srikanthchowdary1994@gmail.com>
2024-06-20 09:53:46 +0530

    shell practice second time practice

--- 05-variable.sh
+++ 05-variable.sh
@@ -2,12 +2,12 @@
 
 echo "Please enter your username::"
 
-read   USERNAME #the value entered above will automatically to USERNAME variable
+read -s  USERNAME #the value entered above will automatically to USERNAME variable
 
+#By using read we can pass names in the run time
+#By -s we can keep the input data masking which we are passing in run time 
 
 echo "Please enter your Password::"
 read -s  PASSWORD #the value entered above will automatically to PASSWORD variable
-
-
 
 echo "Username is: $USERNAME, Password is: $PASSWORD" #i am printing just for validation you should not print username ans passwords in scripts
